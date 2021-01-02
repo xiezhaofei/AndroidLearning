14732ddddfada6b87db5b46200e2cd0ff8691b91
xiezhaofei <xiezhaofei.justin@bytedance.com>
2021-01-03 01:35:26 +0800

    权限申请

---
+++ app/src/main/java/com/android/androidlearning/learningcode/permissions/PermissionFragment.kt
@@ -0,0 +1,4 @@
+package com.android.androidlearning.learningcode.permissions
+
+class PermissionFragment {
+}
---
+++ app/src/main/java/com/android/androidlearning/learningcode/permissions/TestPermissionsFragment.kt
@@ -0,0 +1,12 @@
+package com.android.androidlearning.learningcode.permissions
+
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.android.androidlearning.learningcode.fragment.BaseFragment2
+
+class PermissionsFragment : BaseFragment2() {
+    override fun initViews() {
+        super.initViews()
+        addButton("请求camera权限") { TODO("Not yet implemented") }
+    }
+}
